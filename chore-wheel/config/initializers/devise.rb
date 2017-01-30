7c62710416ff429ab9bc4460b9bccb2bfb504981
amrisha-vaidya <amrisha.vaidya@gmail.com>
2017-01-30 16:02:02 -0500

    removed omniauth stuff

--- chore-wheel/config/initializers/devise.rb
+++ chore-wheel/config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'user,public_repo'
+  # config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
