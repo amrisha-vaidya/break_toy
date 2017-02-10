99ad50dcdfb329c4244cb74ff2ff404c2d540bfb
amrisha-vaidya <amrisha.vaidya@gmail.com>
2017-02-09 20:04:06 -0500

    fixed typo

--- chore-wheel/app/models/profile.rb
+++ chore-wheel/app/models/profile.rb
@@ -1,3 +1,3 @@
 class Profile < ApplicationRecord
-  belong_to :user
+  belongs_to :user
 end
--- chore-wheel/db/seeds.rb
+++ chore-wheel/db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Chore.create(
+title: "Take out trash",
+description:"Trash day is thursdays and mondays, please take out trash by 7 pm night before",
+created_at: Time.now,
+updated_at: Time.now
+)
+
+Chore.create(
+title: "Clean the common area",
+description:"Please put away all board games and vacumm the area",
+created_at: Time.now,
+updated_at: Time.now
+)
+
+Chore.create(title: "Clean Up Bathroom",
+description:"Gloves and cleaning materials are under the sink",
+created_at: Time.now,
+updated_at: Time.now
+)
+
+Chore.create(title: "Take out cat litter",
+description:"Clean up the litter box every wednesday",
+created_at: Time.now,
+updated_at: Time.now
+)
+
+Chore.create(title: "Swiffer Kitchen area",
+description:"Swiffer is in the cupboard, please mop the floor with it.",
+created_at: Time.now,
+updated_at: Time.now
+)
