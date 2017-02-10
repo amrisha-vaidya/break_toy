10cd97484724365bc3c8a488dce5a7bdf67a08b6
amrisha-vaidya <amrisha.vaidya@gmail.com>
2017-02-10 11:24:20 -0500

    devise sign up did not have phone attribute

--- chore-wheel/app/controllers/application_controller.rb
+++ chore-wheel/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :email, :password])
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :email, :password, :avatar])
     end
 
